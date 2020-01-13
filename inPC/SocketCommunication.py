13dee3f787f43e1fab9879048ba1b618324680b2
koichirokato <k.kato@tvisioninsights.com>
2020-01-13 20:29:36 +0900

    update

--- inPC/SocketCommunication.py
+++ inPC/SocketCommunication.py
@@ -1,37 +1,44 @@
 import socket
+import time
 
 class SocketCommunication:
-    def __init__(self):
+    def __init__(self, ip_address, port):
         self.s          = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.Ip_address = 1
-        self.Port = 8000
+        self.ip_address = ip_address
+        self.Port       = port
     
     def socket_server_up(self):
-        self.s.bind(("localhost", 50007))    # 指定したホスト(IP)とポートをソケットに設定
+        self.s.bind((self.ip_address, self.Port))    # 指定したホスト(IP)とポートをソケットに設定
         self.s.listen(1)                     # 1つの接続要求を待つ
         self.s, addr = self.s.accept()       # 要求が来るまでブロック
-        print("Conneted by"+str(addr))  #サーバ側の合図
+        print("Conneted by"+str(addr))       #サーバ側の合図
 
     def socket_client_up(self):
-        self.s.connect(("localhost", 50007))
+        while True:
+            try:
+                self.s.connect((self.ip_address, self.Port))
+                print("connection success")
+                break
+            except ConnectionRefusedError:
+                # 接続先のソケットサーバが立ち上がっていない場合、
+                # 接続拒否になることが多い
+                print('Refused Error')
+                print('retry in 5seconds')
+                for i in range(5):
+                    print(str(5-i) + "...")
+                    time.sleep(1)
 
     def socket_com_client(self, data):
         recv_data = self.s.recv(1024)
         recv_data = recv_data.decode()
-        print("Server>", recv_data)        # サーバー側の書き込みを表示
-        # send_data = input("Client>")     # 入力待機
         send_data = data
-        self.s.send(send_data.encode())  # ソケットに入力したデータを送信
+        self.s.send(send_data.encode())
 
-        if send_data == "q":               # qが押されたら終了
-            self.s.close()
+        return recv_data
 
     def socket_com_server(self, data):
-        # send_data = input("Server>")     # 入力待機(サーバー側)
         send_data = data
-        self.s.send(send_data.encode())    # ソケットにデータを送信
-        recv_data = self.s.recv(1024)      # データを受信（1024バイトまで）
+        self.s.send(send_data.encode())
+        recv_data = self.s.recv(1024)
         recv_data = recv_data.decode()
-        print("Client>",recv_data)         # サーバー側の書き込みを表示
-        if recv_data == "q":               # qが押されたら終了
-            self.s.close()
+        return recv_data
