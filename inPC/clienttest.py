384319ebe90ab906d03028fd4298c46db6c3026c
koichirokato <k.kato@tvisioninsights.com>
2020-01-13 18:07:03 +0900

    update socket communication test files

--- inPC/clienttest.py
+++ inPC/clienttest.py
@@ -1,6 +1,21 @@
 import SocketCommunication
+import json
+import datetime
 
 socket = SocketCommunication.SocketCommunication()
 
 socket.socket_client_up()
-socket.socket_com_client("1111")
+
+now = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+
+send_data_dict = {"time":now,"x":10,"y":10,"cansat id":2}
+send_data_json = json.dumps(send_data_dict)
+
+recv_data = socket.socket_com_server(send_data_json)
+
+recv_data_dict = json.loads(recv_data)
+
+print("#####################")
+for ele in recv_data_dict:
+    print(str(ele) + str(recv_data_dict[ele]))
+print("#####################")
